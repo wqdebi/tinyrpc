2c93da940170a20582a392dad837f38525884545
wqdebi <57791141+wqdebi@users.noreply.github.com>
2024-01-20 19:54:20 +0800

    Add files via upload

---
+++ rocket/rocket/commen/mutex.h
@@ -0,0 +1,52 @@
+#ifndef ROCKER_COMMON_MUTEX_H
+#define ROCKER_COMMON_MUTEX_H
+
+#include<pthread.h>
+
+namespace rocket{
+
+template<class T>
+class ScopeMutex{
+private:
+    T& m_mutex;
+    bool m_is_lock{false};
+public:
+    ScopeMutex(T& mutex): m_mutex(mutex){
+        m_mutex.lock();
+        m_is_lock = true;
+    }
+    ~ScopeMutex(){
+        m_mutex.unlock();
+        m_is_lock = false;
+    }
+    void lock(){
+        if(!m_is_lock)
+            m_mutex.lock();
+    }
+    void unlock(){
+        if(m_is_lock){
+            m_mutex.unlock();
+        }
+    }
+};
+
+class Mutex{
+public:
+    Mutex(){
+        pthread_mutex_init(&m_mutex, NULL);
+    }
+    ~Mutex(){
+        pthread_mutex_destroy(&m_mutex);
+    }
+    void lock(){
+        pthread_mutex_lock(&m_mutex);
+    }
+    void unlock(){
+        pthread_mutex_unlock(&m_mutex);
+    }
+private:
+    pthread_mutex_t m_mutex;
+};
+
+}
+#endif
