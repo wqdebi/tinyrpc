57b7094eec16bcb6b82825ec689766427ffa16db
wqdebi <57791141+wqdebi@users.noreply.github.com>
2024-01-25 20:38:50 +0800

    Add files via upload

--- rocket/testcases/test_tcp.cc
+++ rocket/testcases/test_tcp.cc
@@ -1,9 +1,19 @@
 #include"../rocket/common/log.h"
 #include"../rocket/net/tcp/net_addr.h"
 #include<iostream>
+#include"rocket/net/tcp/tcp_server.h"
+#include<memory>
+
+void test_tcp_server(){
+    rocket::IPNetAddr::s_ptr addr = std::make_shared<rocket::IPNetAddr>("127.0.0.1", 12345);
+    DEBUGLOG("create addr %s", addr->toString().c_str());
+    rocket::TcpServer tcp_server(addr);
+    tcp_server.start();
+}
+
 int main(){
     rocket::Config::SetGlobalConfig("../conf/rocket.xml");
     rocket::Logger::InitGlobalLogger();
-    rocket::IPNetAddr addr("127.0.0.1", 12345);
-    DEBUGLOG("create addr %s", addr.toString().c_str());
+    test_tcp_server();
+    
 }
